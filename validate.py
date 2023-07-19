694bb67af72c0eddd96b1ee989a49b273f2f3020
Rahul CK <rahul@fullstack-wp.com>
2023-07-19 17:12:53 +0545

    code files

---
+++ validate.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+
+class Predict():
+    def __init__(self) -> None:
+        self.model = load_model('./model/model.h5')
+        # self.image = image
+        self.class_indices = ['+',
+                              '-',
+                              '0',
+                              '1',
+                              '2',
+                              '3',
+                              '4',
+                              '5',
+                              '6',
+                              '7',
+                              '8',
+                              '9',
+                              '=',
+                              'X',
+                              'Y',
+                              'Z']
+
+    def get_class(self, img):
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = x.astype('float32')/255
+        z = self.model.predict(x)
+        pred = np.argmax(z)
+        return self.class_indices[pred]
